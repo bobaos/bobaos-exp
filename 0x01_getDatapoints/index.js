2cc50d1c1b19cc18053f3f8070745fb209512f33
Vladimir Shabunin <va.shabunin@physics.msu.ru>
2018-05-07 02:43:25 +0300

    getDatapoints

---
+++ 0x01_getDatapoints/index.js
@@ -0,0 +1,17 @@
+const BdsdClient = require('bdsd.client');
+
+// BdsdClient function accepts socket filename as an argument. 
+// If no argument provided then it will try to connect to following file:
+// process.env['XDG_RUNTIME_DIR'] + '/bdsd.sock'. Usually it is /run/user/1000/bdsd.sock.
+let myClient = BdsdClient();
+
+myClient.on('connect', _ => {
+  console.log('client connected');
+  myClient
+    .getDatapoints()
+    .then(payload => {
+       console.log('All configured datapoints:');
+       console.log(payload.map(t => { return {id: t.id, dpt: t.dpt}}));
+    })
+    .catch(console.log);
+});
