5eaa4b5a498f393ba06d60f36781ac2ec364a944
Vladimir Shabunin <va.shabunin@physics.msu.ru>
2018-05-07 03:46:07 +0300

    Restructuring

---
+++ 0x02_readGetSetValue/index.js
@@ -0,0 +1,15 @@
+const BdsdClient = require('bdsd.client');
+
+// BdsdClient function accepts socket filename as an argument. 
+// If no argument provided then it will try to connect to following file:
+// process.env['XDG_RUNTIME_DIR'] + '/bdsd.sock'. Usually it is /run/user/1000/bdsd.sock.
+let myClient = BdsdClient();
+
+myClient.on('connect', _ => {
+  console.log('client connected');
+});
+
+// Register listener for broadcasted values
+myClient.on('value', data => {
+  // console.log('broadcasted value', data);
+});
