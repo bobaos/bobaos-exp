264fdbbd0b04fbd53b34dc008068abe7d2c77371
Vladimir Shabunin <va.shabunin@physics.msu.ru>
2018-05-13 23:19:33 +0000

    0x02_readGetSet example

--- 0x02_readGetSetValue/index.js
+++ 0x02_readGetSetValue/index.js
@@ -7,8 +7,27 @@ let myClient = BdsdClient();
 
 myClient.on('connect', _ => {
   console.log('client connected');
+  // get all dpts
+  myClient
+    .getDatapoints()
+    .then(payload => {
+      console.log('All configured datapoints:');
+      payload.forEach(t => {
+        let f = t.flags;
+        let output = `#${t.id} [${t.dpt}], priority: ${f.priority}, flags:`;
+        output += `${f.communication ? 'C' : '_'}${f.read ? 'R' : '_'}`;
+        output += `${f.write ? 'W' : '_'}${f.transmit ? 'T' : '_'}`;
+        console.log(output);
+        // send readRequests. should have update flag configured
+        if (f.update) {
+          myClient
+            .readValue(t.id)
+            .catch(console.log);
+        }
+      });
+    });
 
-  // invert datapoint value as fast as possible
+  // invert datapoint value every ~second
   let sw = _ => {
     myClient
       .getValue(9)
@@ -17,7 +36,7 @@ myClient.on('connect', _ => {
           .setValue(payload.id, !payload.value)
           .then(_ => {
             console.log(`switching datapoint ${payload.id} to ${!payload.value} successful`);
-            sw();
+            setTimeout(sw, 1000);
           })
           .catch(console.log);
       })
@@ -26,7 +45,16 @@ myClient.on('connect', _ => {
   sw();
 });
 
-// Register listener for broadcasted values
-myClient.on('value', data => {
-  // console.log('broadcasted value', data);
+// listen to incoming values
+myClient.on('value', _ => {
+  let {id, value} = _;
+  console.log(`#${id} has been changed to ${value}`);
+  // now some conditions
+  if (id === 42) {
+    myClient.setValue(43, !value)
+      .then(_ => {
+        console.log(`switching datapoint ${_.id}: success`);
+      })
+      .catch(console.log);
+  }
 });
